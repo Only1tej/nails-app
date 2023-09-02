eab979fb556ef662d4038e8aa5e64b7e4b946ee3
only1tej <tijanienny3@gmail.com>
2023-09-02 12:28:07 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-2xl italic">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
